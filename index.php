29305cd95379482de7d0eec0efdfedd65ff43d84
Seth Ammons <seth.ammons@sendgrid.com>
2012-11-01 14:38:21 -0700

    adjusted look of landing page

--- index.php
+++ index.php
@@ -2,19 +2,33 @@
 <head>
     <title>CountMyReps</title>
     <style  type="text/css">
+        body{
+            background-color: #E8E8E8;
+        }
         div.center{
             margin: auto;
+            background-color: white;
             background-image: url(images/mustache.jpg); 
-            height: 200px; 
-            width: 400px; 
-            border: 1px solid black;     
+            background-repeat: no-repeat;
+            background-position: center;
+            height: 500px; 
+            width: 800px; 
+            border: 1px solid #C8C8C8; 
+       }
+       div.inner{
+            margin: auto;
+            text-align: center;
+            padding-top: 300px;
+            color: #666362;
        }
     </style>
 </head>
 <body>
 
 <div class="center">
- Rep Counter Coming Soon.
+    <div class="inner">
+         Rep Counter Coming Soon.
+    </div>
 </div>
 
 </body>
