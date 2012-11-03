06ec9d74b31c1dbe79f4b7a0375a390a0df18037
Seth Ammons <seth.ammons@sendgrid.com>
2012-11-03 10:46:48 -0700

    make sure that get_data is working

--- parseapi/get_data.php
+++ parseapi/get_data.php
@@ -1,9 +1,57 @@
 <?php
+include("../includes/DataStore.php");
+
+$DataStore = new DataStore;
+$content = '';
 
 $user = $_POST['email'];
-echo "Looking up info for $user";
+
+if ($DataStore->user_exists($user)){
+    $content = "User Found";
+}
+else{
+    $content = "No User Found";
+}
+
 
 // look up user
 // if found, display reps
 
 // else show error
+
+?>
+<html>
+<head>
+    <title>CountMyReps</title>
+    <style  type="text/css">
+        body{
+            background-color: #E8E8E8;
+        }
+        div.center{
+            margin: auto;
+            background-color: white;
+            width: 800px; 
+            border: 1px solid #C8C8C8; 
+            padding-top: 10px;
+            padding-bottom: 20px;
+       }
+       div.inner{
+            margin: auto;
+            text-align: center;
+            padding-top: 300px;
+            color: #666362;
+       }
+    </style>
+</head>
+<body>
+
+<div class="center">
+    <div class="inner">
+    <?php
+        echo $content;
+    ?>
+    </div>
+</div>
+
+</body>
+</html>
