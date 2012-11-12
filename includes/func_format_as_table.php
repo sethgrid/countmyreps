41f7ee0b12d385f76f92bc7a607cde3204c0a704
Seth Ammons on BlueHost <seth@sethammons.com>
2012-11-12 13:56:06 -0700

    calendars are now evenly displaying

--- includes/func_format_as_table.php
+++ includes/func_format_as_table.php
@@ -6,6 +6,19 @@
  * @return string              an html table
  */
 function format_as_table($all_records){
+
+    // initialize empty array
+    $init_array = Array();
+    for ($d = 1; $d <= date('d'); $d++){
+        // add leading zero if needed
+	if ($d < 10) $d = "0$d";
+	// make the key values equal to dates
+	$init_array[date('Y-m-') . $d] = array('situps' => 0, 'pushups' => 0, 'pullups' => 0);
+    }
+
+    // merge to arrays together, over writing with records passed in
+    $all_records = array_merge($init_array, $all_records);
+
     $content = "<table class='data'><th>Date/Time</th><th>Sit-ups</th><th>Push-ups</th><th>Pull-ups</th>";
     foreach ($all_records as $date => $exercises_array){
         $content .= "<tr><td>$date</td>";
