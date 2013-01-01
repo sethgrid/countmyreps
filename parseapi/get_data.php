552c03a00bb7aac56d10ede013ce792cce36ad71
Seth Ammons <seth.ammons@sendgrid.com>
2013-01-01 11:56:16 -0800

    added new hampshire as an office

--- parseapi/get_data.php
+++ parseapi/get_data.php
@@ -17,10 +17,12 @@ if ($user_id){
     $person_count_california_real = 34;
     $person_count_boulder_real    = 48;
     $person_count_denver_real     = 26;
+    $person_count_nh_real         =  2;
 
     $person_count_california_participating = $DataStore->get_count_by_office('california');
     $person_count_boulder_participating    = $DataStore->get_count_by_office('boulder');
     $person_count_denver_participating     = $DataStore->get_count_by_office('denver');
+    $person_count_nh_participating         = $DataStore->get_count_by_office('nh');
 
     $header  = "<h3>Reps for $user</h3>";
 
@@ -28,29 +30,34 @@ if ($user_id){
     $all_records_california = $DataStore->get_all_records_by_office('california');
     $all_records_boulder    = $DataStore->get_all_records_by_office('boulder');
     $all_records_denver     = $DataStore->get_all_records_by_office('denver');
+    $all_records_nh         = $DataStore->get_all_records_by_office('nh');
 
     $user_reps_table       = format_as_table($all_records_user);
     $california_reps_table = format_as_table($all_records_california);
     $boulder_reps_table    = format_as_table($all_records_boulder);
     $denver_reps_table     = format_as_table($all_records_denver);
+    $nh_reps_table         = format_as_table($all_records_nh);
 
     // get total reps for the offices and the user
     $your_totals       = get_totals($all_records_user);
     $california_totals = get_totals($all_records_california);
     $boulder_totals    = get_totals($all_records_boulder);
     $denver_totals     = get_totals($all_records_denver);
+    $nh_totals         = get_totals($all_records_nh);
 
     $grand_total      = 0;
     $grand_total      = array_sum($california_totals); 
     $grand_total     += array_sum($boulder_totals); 
     $grand_total     += array_sum($denver_totals);
+    $grand_total     += array_sum($hn_totals);
 
     $header .= 'Company total: ' . $grand_total;
 
-    $info_u  = show_stats("Your",       $your_totals, 1, 1);
-    $info_ca = show_stats("California", $california_totals, $person_count_california_real, $person_count_california_participating);
-    $info_co = show_stats("Boulder",    $boulder_totals,    $person_count_boulder_real,    $person_count_boulder_participating);
-    $info_dn = show_stats("Denver",     $denver_totals,     $person_count_denver_real,     $person_count_denver_participating);
+    $info_u  = show_stats("Your",          $your_totals, 1, 1);
+    $info_ca = show_stats("California",    $california_totals, $person_count_california_real, $person_count_california_participating);
+    $info_co = show_stats("Boulder",       $boulder_totals,    $person_count_boulder_real,    $person_count_boulder_participating);
+    $info_dn = show_stats("Denver",        $denver_totals,     $person_count_denver_real,     $person_count_denver_participating);
+    $info_nh = show_stats("New Hampshire", $nh_totals,         $person_count_nh_real,         $person_count_nh_participating);
 
     if ($user == "none"){
 	$info_u = '';
@@ -119,6 +126,7 @@ else{
 		<td class="cell"><?php echo $info_ca.$california_reps_table;?></td>
 		<td class="cell"><?php echo $info_co.$boulder_reps_table;?></td>
 		<td class="cell"><?php echo $info_dn.$denver_reps_table;?></td>
+		<td class="cell"><?php echo $info_nh.$nh_reps_table;?></td>
 	</tr>
 	</table>
     </div>
